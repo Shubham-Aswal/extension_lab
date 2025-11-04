e8c578b7f8d5f0b9d34da9586b93e83980d85376
vedamlab <mitali.rahangdale@vedam.org>
2025-11-04 22:42:57 +0530

    Added new code / Updated feature

--- notes.md
+++ notes.md
@@ -2,24 +2,36 @@
 
 ## Modules
 
-### Module-1 -> Setting up extensions  UI -> HTML and css
-### Module-2 -> Adding intercativity
-### Module 3 : Commuication b/w different part fo chrome extsnion
-### Module 4: Manullay blocking websites
-### Module 5: Adding AI 
+* `Module-1`: Setting up extensions  UI -> HTML and css
+* `Module-2`:  CSS/Adding interactivity
+* `Module 3`: Communication b/w different parts of chrome extension
+* `Module 4`: Manually blocking websites
+* `Module 5`: Adding AI 
+
+### Module 1
+* Extension -> chrome extension, vscode
+* json -> data  format
+* manifest.json -> this is the file used by  chrome to let it know what kind of functionality / permission your extension will be required
+    * You will need to know which keys and values are `key : value -> chrome`
+* UI 
+    * html file -> show UI
+    * css file for -> styling
+
+## Module 2
+* pseduo classes : hover , focus , checked -> if elese stamenets in css
+* transition , transform -> translate
+* circle
+* how to beatify your inputs 
+
+## JS topic
+* variables& datatypes
+cell
+* if else 
+* loops
+* functions
+* arrays 
 
 
-
-
-
-## module 1
-
-* extension -> chrome extension, vscode
-* json -> the format
-* manifest.json -> this is the file used by  by chrome to let it know what kind of functionality / permission your extension will be required
-*. You need to know which keys and values are `key : value -> chrome`
-
-*. html file -> show UI, css
 
 
 
---
+++ poc/script.js
@@ -0,0 +1,88 @@
+// I am comment
+// console.log("Hello from JS:)");
+// let a1;
+
+// a1=10;
+// a1=true;
+// a1="vedam";
+// console.log("value",a1);
+
+
+// let condition=0;
+// if(condition){
+//     console.log("condition is true");
+// }else{
+//     console.log("condition if false");
+// }
+
+
+// for(let i=0;i<=10;i++){
+//     console.log("Number is",i);
+// }
+/*********************function
+ * declare the function => with the code
+ * you can call the function
+ * pass some params
+ * retrun value
+ * *******************/ 
+// console.log("Before")
+// function myFN(){
+// // code
+// console.log("I am a fn");
+// }
+
+// // call
+// console.log("before calling");
+// myFN()
+// console.log("After calling");
+
+// function withParams(param){
+// console.log("param",param)
+// }
+
+// let a=10;
+// withParams(a);
+
+// let a;
+// function withParams(param,param1) {
+//     console.log("param", param,"param1", param1)
+// }
+// // let a=10;
+// let b= 20;
+// withParams(a,b);
+// // console.log(a);
+
+// console.log("after")
+
+// udefined -> type -> JS 
+
+// not defined -> error
+
+
+
+// function myFn(){
+//     console.log("function was called");
+//     let rName="Jasbir"
+//     // return rName;
+// }
+
+// let reVal=myFn();
+// console.log("return values ",reVal);
+
+// let arr=[10,20,30,40,50];
+// let strArr=["value","hello","hi"];
+
+// console.log("arr",arr);
+// console.log("stringArr",strArr);
+
+// console.log("arr length",arr.length);
+// console.log("strArr length",strArr.length);
+
+let arr=[10,20,30,40,50];
+for(let i=0;i<arr.length;i++){
+    console.log("item at",i,"value",arr[i]);
+}
+
+// check if prime
+// find second largest in an array
+// function return if a number if prime
